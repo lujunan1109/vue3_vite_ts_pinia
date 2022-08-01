ec035006f9f673c4cb679c59e36a38936a648b37
lujunan <2833845025@qq.com>
2022-08-01 17:24:56 +0800

    放大镜效果实现

--- src/pages/home-page.vue
+++ src/pages/home-page.vue
@@ -1,50 +1,124 @@
 <!--
- * @Description: 首页
  * @Author: lujunan
- * @Date: 2022-06-07 10:14:22
+ * @Date: 2022-07-12 18:03:34
  * @LastEditors: lujunan
- * @LastEditTime: 2022-07-18 17:43:41
+ * @LastEditTime: 2022-08-01 17:23:34
+ * @Description: 放大镜
 -->
+<script lang="ts" setup name="GoodsImage">
+import { ref } from 'vue';
+import { useMouseInElement, useLocalStorage } from '@vueuse/core';
+import { computed } from 'vue';
+const target = ref(null);
+// isOutside是否进入指定区域 进入为false 否则为true
+// elementX 鼠标X位置
+// elementY 鼠标Y位置
+const { isOutside, elementX, elementY } = useMouseInElement(target); // useMouseInElement(指定的区域)鼠标进入的位置
 
-<template>
-    <div ref="domRef">首页</div>
-</template>
+useLocalStorage('key', 'value');
 
-<script setup lang="ts">
-import { ref, nextTick } from 'vue';
-const domRef = ref(null);
-nextTick(() => {
-    console.log(domRef.value);
+const images = ref(
+    'https://images.mepai.me/app/works/178221/2022-07-14/w_62d01aa163e45/062d01aa163f41.jpg!1200w.jpg',
+);
+const position = computed(() => {
+    // 根据鼠标的位置控制遮罩层的位置移动范围
+    let x = elementX.value - 100;
+    let y = elementY.value - 100;
+    x = x < 0 ? 0 : x;
+    y = y < 0 ? 0 : y;
+    x = x > 200 ? 200 : x;
+    y = y > 200 ? 200 : y;
+    return {
+        x,
+        y,
+    };
 });
 </script>
 
-<style lang="scss" scoped>
-// @import "xxx";
-// @import url(xxx);
-.home {
-    width: 200px;
-    height: 200px;
-    border: 1px solid #ddd;
-    cursor: pointer;
-}
+<template>
+    <!-- {{isOutside}}
+   X: {{elementX}}
+   Y: {{elementY}} -->
+    <div class="goods-image">
+        <!-- 显示在右侧的放大之后的区域 -->
+        <div
+            v-show="!isOutside"
+            class="large"
+            :style="[
+                {
+                    backgroundImage: 'url(' + images + ')',
+                    backgroundPosition: `-${position.x * 2}px -${
+                        position.y * 2
+                    }px`,
+                },
+            ]"
+        ></div>
 
-.style {
-    margin: auto;
-    width: 120px;
+        <div ref="target" class="middle">
+            <img :src="images" alt="" />
+            <!-- 移动遮罩 -->
+            <div
+                v-show="!isOutside"
+                class="layer"
+                :style="{ left: position.x + 'px', top: position.y + 'px' }"
+            ></div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.goods-image {
+    width: 480px;
+    height: 400px;
+    position: relative;
     display: flex;
-    justify-content: space-between;
-    i {
-        font-size: 30px;
-        display: block;
-        // color: #00aeec;
-        color: #61666d;
+    z-index: 500;
+    .large {
+        position: absolute;
+        top: 0;
+        left: 412px;
+        width: 400px;
+        height: 400px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        background-repeat: no-repeat;
+        // 放大一倍
+        background-size: 800px 800px;
+        background-color: #f8f8f8;
     }
-}
-
-.color {
-    i {
-        color: #00aeec;
+    .middle {
+        // 初始化不让放大镜出现
+        width: -400px;
+        height: -400px;
+        background: #f5f5f5;
+        position: relative;
+        cursor: move;
+        img {
+            width: 400px;
+            height: 400px;
+        }
+        .layer {
+            width: 200px;
+            height: 200px;
+            background: rgba(0, 0, 0, 0.2);
+            left: 0;
+            top: 0;
+            // 可以移动
+            position: absolute;
+        }
     }
-    transition: color 0.5s cubic-bezier(0.86, 0, 0.07, 1);
+    .small {
+        width: 80px;
+        li {
+            width: 68px;
+            height: 68px;
+            margin-left: 12px;
+            margin-bottom: 15px;
+            cursor: pointer;
+            &:hover,
+            &.active {
+                border: 2px solid red;
+            }
+        }
+    }
 }
 </style>
